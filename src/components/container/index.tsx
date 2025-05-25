b8ac921cf44b8605f8c7eb7f91ae215c400bbcb9
Luke Charles <info@readingloopai.com>
2025-05-25 15:20:32 +0100

    Updated the theme vars for the breakpoints and container sizes (to be like bootstrap), updated the classes on the container component for the max widths

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -1,6 +1,6 @@
 export default function Container({ children }: { children: React.ReactNode }) {
   return (
-    <div className="mx-auto px-3 w-full">
+    <div className="mx-auto px-3 w-full max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl 2xl:max-w-2xl">
       {children}
     </div>
   )
