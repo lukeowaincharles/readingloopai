b3c61a88c69c7cbfbac2a83eb1568976d4be345b
Luke Charles <info@readingloopai.com>
2025-05-25 19:07:43 +0100

    Fixed layout of email and socials on mobile

--- src/slices/Contact/index.tsx
+++ src/slices/Contact/index.tsx
@@ -23,11 +23,11 @@ const Contact: FC<ContactProps> = ({ slice }) => {
         <div className="flex flex-col items-center justify-center">
           <h2 className="text-xl font-bold pb-3">{slice.primary.title}</h2>
           <p className="pb-5">{slice.primary.description}</p>
-          <div className="flex items-center">
+          <div className="flex items-center flex-col md:flex-row">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-envelope mr-2 text-teal-800" viewBox="0 0 16 16">
               <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z" />
             </svg>
-            <PrismicNextLink className="text-teal-800 hover:text-gray-950 hover:underline border-r-2 border-gray-950 pr-4 mr-4" field={slice.primary.email} />
+            <PrismicNextLink className="text-teal-800 hover:text-gray-950 hover:underline border-gray-950 border-b-2 pb-4 mb-4 md:border-b-0 md:border-r-2 md:pr-4 md:pb-0 md:mb-0 md:mr-4" field={slice.primary.email} />
             <p className="text-sm pr-2">Follow our journey:</p>
             {slice.primary.socials.map((item, i) => {
               const icon = () => {
