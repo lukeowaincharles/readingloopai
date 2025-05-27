bdc26872edc32f1bf78e25ce80539f8cc4c8be95
Luke Charles <info@readingloopai.com>
2025-05-27 13:00:03 +0100

    Fixed a tag to use Link, removed unused declaration

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,4 +1,5 @@
 import Container from "@/components/container";
+import Link from "next/link";
 
 export default function Footer() {
   const startYear = 2025;
@@ -11,7 +12,7 @@ export default function Footer() {
         <div className="flex flex-col items-center justify-center">
           {/* @TODO: Add icon here */}
           <p>Reading Loop</p>
-          <p className="text-xs mt-2 text-zinc-300">© {yearRange} Reading Loop. All Rights Reserved. <a href="/privacy-policy" className="underline">Privacy Policy</a></p>
+          <p className="text-xs mt-2 text-zinc-300">© {yearRange} Reading Loop. All Rights Reserved. <Link href="/privacy-policy" className="underline">Privacy Policy</Link></p>
         </div>
       </Container>
     </footer>
